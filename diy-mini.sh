0b2f059a7f510826051baeb9b832a4e1178d1004
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-10-30 20:59:00 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -4,8 +4,8 @@
 # sed -i 's/192.168.1.1/192.168.1.251/g' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
-# rm -rf feeds/luci/themes/luci-theme-argon
-# rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
 
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -23,14 +23,11 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice package/
 
 # 科学上网插件
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-# git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+
 
 # Themes
-# git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-# git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 
 # 更改 Argon 主题背景
